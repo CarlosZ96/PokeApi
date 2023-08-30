4abc4a9063024b6510e3d7ed36dd78f53c375a7a
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-30 17:57:32 -0500

    Update ProItem.jsx

--- src/components/ProItem.jsx
+++ src/components/ProItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProItem = ({ data, addToCart }) => {
   let { id, name, price } = data;
   return (
-    <div>
+    <div className='products-container'>
      <h4>{name}</h4>
      <h5>${price}.00</h5>
      <button onClick={() => addToCart(id)}>Add</button>
